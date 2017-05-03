6369742fada634a35411ece08cc582a94a4a6b59
Administrator <Administrator@MilkyWay>
2017-05-03 10:17:00 +0200

    verwijderen functie added

---
+++ src/java/Servlets/BoekenServlet.java
@@ -0,0 +1,51 @@
+package Servlets;
+
+import JavaFiles.Reservatie;
+import java.io.IOException;
+import java.util.TreeMap;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet(urlPatterns = {"/boeken"})
+public class BoekenServlet extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+    private static final String VIEW = "WEB-INF/jsp/boeken.jsp";
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        HttpSession session = request.getSession();
+        @SuppressWarnings("unchecked")
+        TreeMap mandje = (TreeMap) session.getAttribute("mandje");
+
+        session.setAttribute("mandje", mandje);
+        request.getRequestDispatcher(VIEW).forward(request, response);
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        HttpSession session = request.getSession();
+        @SuppressWarnings("unchecked")
+        TreeMap mandje = (TreeMap) session.getAttribute("mandje");
+        double totalPrice = (double) session.getAttribute("totalPrice");
+        
+        String type = request.getParameter("id");
+        Reservatie reservatie = (Reservatie) mandje.get(type);
+            
+        totalPrice -= (Double.parseDouble(reservatie.getVoorstelling().getPrijs())*reservatie.getAantalTickets());
+        mandje.remove(type);
+
+        session.setAttribute("totalPrice", totalPrice);
+        session.setAttribute("mandje", mandje);
+        request.getRequestDispatcher(VIEW).forward(request, response);
+    }
+}
